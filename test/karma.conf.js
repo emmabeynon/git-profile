e0a924d6d73f49a5c95b4e3a244963df82b3a4dc
emmabeynon <emma.beynon@gmail.com>
2016-01-26 12:43:20 +0000

    Created searchResult property and doSearch function in the controller.  Amended Search button to trigger doSearch on click.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -63,6 +63,8 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    usePolling: true
   });
 };
